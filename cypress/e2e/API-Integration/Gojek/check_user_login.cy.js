ba951b1d874a9b498eb93aef4cb3dab28b11c945
rnggptr10 <ranggaputra103@gmail.com>
2023-10-09 12:12:59 +0700

    update script check user login

--- cypress/e2e/API-Integration/Gojek/check_user_login.cy.js
+++ cypress/e2e/API-Integration/Gojek/check_user_login.cy.js
@@ -1,9 +1,41 @@
 describe("Check User Login", () => {
-  it("tanpa field email yg required", () => {
+  const userData = {
+    email: "agentccu1@uatgojek.com",
+    password: "gojek123",
+  };
 
+  before("do login", () => {
+    cy.request({
+      method: "POST",
+      url: "https://api.sociomile.net/auth/login",
+      body: {
+        email: userData.email,
+        password: userData.password,
+      },
+      failOnStatusCode: false,
+    }).then((responses) => {
+      Cypress.env("token", responses.body.token);
+    });
   });
 
-  it("semau field diisi", () => {
-    
+  after("do logout", () => {
+    cy.request({
+      method: "GET",
+      url: "https://api.sociomile.net/logout",
+      headers: {
+        Authorization: `Bearer ${Cypress.env("token")}`,
+      },
+      failOnStatusCode: false,
+    });
+  });
+
+  it("check token after login", () => {
+    cy.request({
+      method: "GET",
+      url: `https://s3-api-ng.sociomile.net/me?token=${Cypress.env("token")}`,
+      failOnStatusCode: false,
+    }).then((responses) => {
+      
+    });
   });
 });
